da831a2afb2604633304eb7c5017227a057b3912
HoMeTown <hometownsocool@gmail.com>
2024-03-22 11:38:58 +0800

    feat: eslint config

---
+++ eslint.config.js
@@ -0,0 +1,12 @@
+// @ts-check
+import antfu from '@antfu/eslint-config'
+
+export default antfu({
+  typescript: true,
+  vue: true,
+  unocss: true,
+  formatters: {
+    css: true,
+    html: true,
+  },
+})
